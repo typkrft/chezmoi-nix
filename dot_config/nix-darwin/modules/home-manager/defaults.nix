eb473d009d913c1dd4b98392d66535888a429cef
typkrft <>
2023-07-20 22:52:49 -0400

    mostly zsh setup

--- dot_config/nix-darwin/modules/home-manager/defaults.nix
+++ dot_config/nix-darwin/modules/home-manager/defaults.nix
@@ -35,6 +35,21 @@
   programs.fzf = {
     enable = true;
     enableZshIntegration = true;
+    colors = {
+      "fg" = "#f8f8f2"; 
+      "fg+" = "#f8f8f2";
+      "bg" = "#282a36"; 
+      "bg+" = "#44475a";
+      "hl" = "#bd93f9"; 
+      "hl+" = "#bd93f9";
+      "info" = "#ffb86c";
+      "prompt" = "#50fa7b";
+      "pointer" = "#ff79c6";
+      "marker" = "#ff79c6";
+      "spinner" = "#ffb86c";
+      "header" = "#6272a4";
+    };
+    tmux.enableShellIntegration = true;
   };
 
   programs.git = {
@@ -54,12 +69,37 @@
     enableAliases = true;
     git = true;
     icons = true;
+    extraOptions = [
+      "-a" 
+      "-F" 
+      "--group-directories-first"
+      "-G"
+    ];
   };
 
   programs.zellij = {
     enable = true;
     enableZshIntegration = true;
-    # todo theme
+    
+    settings = {
+      # simplified_ui = flase;
+      pane_frames = false;
+      theme = "dracula";
+      ui.pane_frame.rounded_corners = false;
+
+      #! https://github.com/nix-community/home-manager/issues/4054
+      # NOTE: Alternative link to file eg: home.file.".config/zellij/config.kdl".source = ./config.kdl;
+      # keybinds = {
+      #   normal = {
+      #     bind = [
+      #       {
+      #         _args = [ "Ctrl k = Clear;" ];
+      #       }
+      #     ];
+      #   };
+      # };
+    };
+    
   };
 
 }
--- dot_config/nix-darwin/modules/home-manager/zsh.nix
+++ dot_config/nix-darwin/modules/home-manager/zsh.nix
@@ -1,14 +1,23 @@
 { pkgs, ... }: {
   home = {
-    sessionPath = [ "~/.local/bin" ];
+    # sessionPath = [ "~/.local/bin" ]; #! https://github.com/nix-community/home-manager/issues/2991
     
     sessionVariables = {
       EDITOR = "nvim";
+      AUTO_NOTIFY_THRESHOLD = "20";
     };
 
     shellAliases = {
       dots = "chezmoi cd; code .";
       u-nix = "chezmoi apply -v && darwin-rebuild switch --flake $HOME/.config/nix-darwin/.#";
+      g-nix = "nix-collect-garbage -d && sudo nix-collect-garbage -d";
+      o-nix = "nix --extra-experimental-features nix-command store optimise";
+      ssh = "TERM=xterm-256color ssh";
+      man = "batman";
+      pip = "noglob pip";
+      cat = "bat";
+      nix = "nix --extra-experimental-features 'nix-command flakes'";
+      rm = "trash --trash-dir=$HOME/.Trash";
     };
   };
 
@@ -28,8 +37,65 @@
       eval "$(zoxide init zsh)"
       source $(brew --prefix asdf)/libexec/asdf.sh
     '';
+
+    initExtra = ''
+      # fzf-tab
+      zstyle ':completion:*:git-checkout:*' sort false
+      zstyle ':completion:*:descriptions' format '[%d]'
+      zstyle ':completion:*' list-colors ''${(s.:.)LS_COLORS}
+      zstyle ':fzf-tab:complete:cd:*' fzf-preview 'exa -1 --icons -a --group-directories-first --git --color=always $realpath'
+      zstyle ':fzf-tab:*' switch-group ',' '.'
+
+      # Bind Keys
+      bindkey '^[[A' history-beginning-search-backward
+      bindkey '^[[B' history-beginning-search-forward
+      bindkey '^[[1;9D'  beginning-of-line
+      bindkey '^[[1;9C'  end-of-line
+
+      # Autols
+      function report() {
+        DIRS=$(find . -mindepth 1 -maxdepth 1 -type d | wc -l)
+        # HIDDEN_DIRS=$(find . -iname ".*" -mindepth 1 -maxdepth 1 -type d | wc -l)
+        # TOTAL_DIRS=$(( $DIRS + $HIDDEN_DIRS ))
+        
+        FILES=$(find . -mindepth 1 -maxdepth 1 -type f | wc -l)
+        # HIDDEN_FILES=$(find . -iname ".*" -mindepth 1 -maxdepth 1 -type f| wc -l)
+        # TOTAL_FILES=$(( $FILES + $HIDDEN_FILES ))
+        
+        DIR_SIZE=$(timeout 0.5s du -sbh  . 2>/dev/null | awk '{print $1}')
+        if [[ $? -ne 0 ]]; then 
+            DIR_SIZE=""
+        fi
+        
+        KEY_COLOR=$(tput bold; tput setaf 6)
+        VALUE_COLOR=$(tput sitm; tput setaf 2)
+        RESET_TEXT=$(tput sgr0)
+
+        exa --icons -a --group-directories-first --git -F
+        printf "\n"
+        printf "''${KEY_COLOR}Path:''${RESET_TEXT}        ''${VALUE_COLOR}$(pwd)''${RESET_TEXT}\n"
+        printf "''${KEY_COLOR}Folders:''${RESET_TEXT}     ''${VALUE_COLOR}''${DIRS}''${RESET_TEXT}\n"
+        printf "''${KEY_COLOR}Files:''${RESET_TEXT}       ''${VALUE_COLOR}''${FILES}''${RESET_TEXT}\n"
+        if [[ $DIR_SIZE != "" ]]; then 
+            printf "''${KEY_COLOR}Size:''${RESET_TEXT}        ''${VALUE_COLOR}''${DIR_SIZE}''${RESET_TEXT}\n"
+        fi
+      }
+      chpwd() {
+        report
+      }
+
+      # Command-not-found hook
+      HB_CNF_HANDLER="$(brew --repository)/Library/Taps/homebrew/homebrew-command-not-found/handler.sh"
+      if [ -f "$HB_CNF_HANDLER" ]; then
+        source "$HB_CNF_HANDLER";
+      fi
+
+    # Don't Highlight Pasted Content
+    zle_highlight=(paste:none)
+    '';
     
     initExtraBeforeCompInit = ''
+      path+=("''$HOME/.local/bin")
       FPATH="$(brew --prefix)/share/zsh/site-functions:''${FPATH}"
     '';
 
@@ -43,9 +109,52 @@
       share = true;
     };
 
-    historySubstringSearch = {
-      enable = true;
-    };
+    # historySubstringSearch = {
+    #   enable = true;
+    # };
+
+    plugins = [
+      {
+        name = "fzf-tab";
+        file = "fzf-tab.zsh";
+        src = pkgs.fetchgit {
+          url = "https://github.com/Aloxaf/fzf-tab";
+          sha256 = "sha256-gvZp8P3quOtcy1Xtt1LAW1cfZ/zCtnAmnWqcwrKel6w=";
+        };
+      }
+      {
+        name = "zsh-notify";
+        file = "auto-notify.plugin.zsh";
+        src = pkgs.fetchgit {
+          url = "https://github.com/MichaelAquilina/zsh-auto-notify";
+          sha256 = "sha256-x+6UPghRB64nxuhJcBaPQ1kPhsDx3HJv0TLJT5rjZpA=";
+        };
+      }
+      {
+        name = "ssh-completion";
+        file = "zsh-ssh.zsh";
+        src = pkgs.fetchgit {
+          url = "https://github.com/sunlei/zsh-ssh";
+          sha256 = "sha256-nUVIkoF400ZdwR+uKoW8cwG0uN/+HPWQ1k8/VwA5OJQ=";
+        };
+      }
+      {
+        name = "fast-zsh-syntax";
+        file = "fast-syntax-highlighting.plugin.zsh";
+        src = pkgs.fetchgit {
+          url = "https://github.com/zdharma-continuum/fast-syntax-highlighting";
+          sha256 = "sha256-RVX9ZSzjBW3LpFs2W86lKI6vtcvDWP6EPxzeTcRZua4=";
+        };
+      }
+      {
+        name = "zsh-autopair";
+        file = "autopair.zsh";
+        src = pkgs.fetchgit {
+          url = "https://github.com/hlissner/zsh-autopair";
+          sha256 = "sha256-PXHxPxFeoYXYMOC29YQKDdMnqTO0toyA7eJTSCV6PGE=";
+        };
+      }
+    ];
 
   };
 }
